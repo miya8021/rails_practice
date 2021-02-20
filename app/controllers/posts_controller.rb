dbb3decd03f2dcc06c1d44b622eaded875232c2e
miya <rinn.sasami@gmail.com>
2021-02-20 20:34:19 +0900

    詳細表示機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -22,7 +23,7 @@ class PostsController < ApplicationController
 
   def destroy
   end
-  
+
   private
 
   def post_params
